64035b06de3506f749f69bd6be9a73a5250d4697
VitorPrates <vitoraaprates@gmail.com>
2026-06-01 16:37:23 -0300

    Adicionando animação nos botões

--- funks.js
+++ funks.js
@@ -1,4 +1,5 @@
 const cube = document.getElementById('cube');
+let rodando = false
 
 let isDragging = false;
 let previousX = 0;
@@ -36,41 +37,72 @@ window.addEventListener('mousemove', (e) => {
 
 function rodar(pos)
 {
-    let new_pos_x = 0
-    let new_pos_y = 0
-
-    switch (pos) {
-        case 1:
-            rotateX = 0;
-            rotateY = 0;
-            break;
-        case 2:
-            rotateX = 0;
-            rotateY = 180;
-            break;
-        case 3:
-            rotateX = 0;
-            rotateY = 270;
-            break;
-        case 4:
-            rotateX = 0;
-            rotateY = 90;
-            break;
-        case 5:
-            rotateX = -90;
-            rotateY = 0;
-            break;
-        case 6:
-            rotateX = 90;
-            rotateY = 0;
-            break;
-    }
-    function rotacionar()
+    if(!rodando)
     {
+        rodando = true
+        let alvoX = 0;
+        let alvoY = 0;
 
+        switch (pos)
+        {
+            case 1:
+                alvoX = 0;
+                alvoY = 0;
+                break;
+
+            case 2:
+                alvoX = 0;
+                alvoY = 180;
+                break;
+
+            case 3:
+                alvoX = 0;
+                alvoY = 270;
+                break;
+
+            case 4:
+                alvoX = 0;
+                alvoY = 90;
+                break;
+
+            case 5:
+                alvoX = -90;
+                alvoY = 0;
+                break;
+
+            case 6:
+                alvoX = 90;
+                alvoY = 0;
+                break;
+        }
+
+        function animar()
+        {
+            const velocidade = 0.15;
+
+            rotateX += (alvoX - rotateX) * velocidade;
+            rotateY += (alvoY - rotateY) * velocidade;
+
+            cube.style.transform =
+                `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+            const chegouX = Math.abs(alvoX - rotateX) < 0.1;
+            const chegouY = Math.abs(alvoY - rotateY) < 0.1;
+
+            if (!chegouX || !chegouY)
+            {
+                requestAnimationFrame(animar);
+            }
+            else
+            {
+                rotateX = alvoX;
+                rotateY = alvoY;
+                cube.style.transform =
+                    `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+                rodando = false
+            }
+        }
+        requestAnimationFrame(animar);
     }
-    requestAnimationFrame(rotacionar)
-
-    cube.style.transform =
-        `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    
 }
